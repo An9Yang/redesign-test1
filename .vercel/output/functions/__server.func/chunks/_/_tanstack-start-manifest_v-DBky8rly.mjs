9e0df26be1af84d916a549712997dee7a36438ca
AY <ay2494@nyu.edu>
2026-01-28 13:33:28 +0800

    chore: add .vercel to gitignore - build output should be generated on Vercel

--- .vercel/output/functions/__server.func/chunks/_/_tanstack-start-manifest_v-DBky8rly.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "/Users/annanyang/Downloads/redesign-test1/src/routes/__root.tsx", "children": ["/", "/api/trpc/$"], "preloads": ["/assets/main-xVu4on7K.js"], "assets": [] }, "/": { "filePath": "/Users/annanyang/Downloads/redesign-test1/src/routes/index.tsx", "assets": [], "preloads": ["/assets/index-P6W1H70e.js"] }, "/api/trpc/$": { "filePath": "/Users/annanyang/Downloads/redesign-test1/src/routes/api/trpc.$.ts" } }, "clientEntry": "/assets/main-xVu4on7K.js" });
-export {
-  tsrStartManifest
-};
